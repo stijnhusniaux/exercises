52b59ea146f03d01040aff1996815268ed2f0d63
Stijn Husniaux <stijn.husniaux@skynet.be>
2022-02-16 17:32:44 +0100

    loops

--- 01-basic-python/03-loops/01-digit-sum/student.py
+++ 01-basic-python/03-loops/01-digit-sum/student.py
@@ -1,1 +1,10 @@
 # Write your code here
+#`digitSum` computes the sum of all the decimal digits of a given number `n`.
+#For example, `digitSum(159)` returns `15` because 1+5+9=15.
+
+def digitSum(n):
+    result = 0
+    while ( n > 0 ):
+        result += n % 10
+        n //= 10
+    return result
