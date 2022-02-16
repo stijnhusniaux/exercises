996181538d61bfce65fd02f7c03caae63ed8bbdf
Stijn Husniaux <stijn.husniaux@skynet.be>
2022-02-16 17:06:27 +0100

    verdere uitwerking van arithmetic en conditionals

--- 01-basic-python/01-arithmetic/02-square/student.py
+++ 01-basic-python/01-arithmetic/02-square/student.py
@@ -1,1 +1,4 @@
 # Write your code here
+def square(x):
+    result = x ** 2
+    return result
--- 01-basic-python/01-arithmetic/03-cakes/student.py
+++ 01-basic-python/01-arithmetic/03-cakes/student.py
@@ -1,1 +1,9 @@
 # Write your code here
+def cakes(eggs,butter,flour):
+    ##Do NOT rely on floor in your translation
+    ##Use integer division as explained below
+    maxByEggs = eggs // 5
+    maxByButter = butter // 250
+    maxByFlour = flour // 5
+
+    return min(maxByEggs, maxByButter, maxByFlour)
