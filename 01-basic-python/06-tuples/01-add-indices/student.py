b38cdaddb5a9bd9202519ed89e77f39f5a054e74
Stijn Husniaux <stijn.husniaux@skynet.be>
2022-02-21 22:24:56 +0100

    yadayadayada

--- 01-basic-python/06-tuples/01-add-indices/student.py
+++ 01-basic-python/06-tuples/01-add-indices/student.py
@@ -1,1 +1,3 @@
 # Write your code here
+def add_indices(xs):
+    return list(zip(range(len(xs)), xs))
--- 01-basic-python/06-tuples/02-is-increasing/student.py
+++ 01-basic-python/06-tuples/02-is-increasing/student.py
@@ -1,1 +1,10 @@
 # Write your code here
+def is_increasing(ns):
+    for i in  range (0,len(ns)):
+        x = ns[i]
+        y = ns[i + 1]
+
+        if ( x > y ):
+            return False
+        else:
+            return True
